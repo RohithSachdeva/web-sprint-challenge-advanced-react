a7f97d318dc0bac30c431bde5c04563032692598
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-05-08 09:33:56 -0700

    before implementing custom hook fully

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -13,10 +13,11 @@ const initialValue = {
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
+//Delete set state... remove handleChanges (setup in custom hook), modify handlesubmit?  
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -2,15 +2,30 @@ import { useLocalStorage } from "./useLocalStorage"
 
 export const useForm = (key, initialValue) => {
     const [values, setValues] = useLocalStorage(key, initialValue);
+    const handleChanges = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+    const clearForm = (e) => {
+        if (e) e.preventDefault();
+        setValues(initialValue);
+    };
+    return [values, handleChanges, clearForm];
 }
 
 
+//CheckoutForm 
+//  const handleChanges = (e) => {
+//     setValues({ ...values, [e.target.name]: e.target.value });
+//   };
 
 
 
 
-
-
+//Need handleChanges, clearForm ---> return [values, handleChanges, clearForm]
+//utilize setValues and string operator 
 
 
 
